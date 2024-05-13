57b4906fccce54175f8b6007a7338cd97e9b9e26
ElDav1d <dvivoes@yahoo.es>
2024-05-13 11:24:20 +0200

    install test utilities

---
+++ src/atoms/button/Button.test.tsx
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { test, expect } from "vitest";
+import { Button } from "./Button";
+
+test("Button renders", () => {
+  // ACT
+  render(<Button label="testButton" />);
+
+  const button = screen.getByRole("button", { name: "testButton" });
+
+  // ASSERT
+  expect(button).toBeInTheDocument();
+});
