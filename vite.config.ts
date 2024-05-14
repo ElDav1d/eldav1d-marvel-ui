1366a81cf52dd939a896dab4bfb60f6b3273a380
ElDav1d <dvivoes@yahoo.es>
2024-05-14 10:37:19 +0200

    FIx dist output

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import reactSwc from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactSwc()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'eldav1d-marvel-ui',
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          'eldav1d-marvel-ui': 'eldav1d-marvel-ui',
+        },
+      },
+    },
+  },
 });
