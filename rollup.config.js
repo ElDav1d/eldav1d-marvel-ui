c362eb8eb115dbf7151581031a2fcc2e125cd3cd
ElDav1d <dvivoes@yahoo.es>
2024-05-16 10:34:44 +0200

    FIX modules conflict WIP 2

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'dist/eldav1d-marvel-ui.js',
   output: {
-    file: 'dist/eldav1d-marvel-ui.cjs.js',
+    file: 'dist/eldav1d-marvel-ui.js',
     format: 'cjs',
   },
   plugins: [commonjs()],
