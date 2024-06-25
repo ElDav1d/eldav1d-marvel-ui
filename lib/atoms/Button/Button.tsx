d23f5d1ee9561d1a054567509ea75d11014a11fd
ElDav1d <dvivoes@yahoo.es>
2024-06-25 17:04:15 +0200

    style button

--- lib/atoms/Button/Button.tsx
+++ lib/atoms/Button/Button.tsx
@@ -1,13 +1,15 @@
 export interface IButtonProps {
-  children: React.ReactNode;
+  /**
+   * @property {React.ReactNode}
+   * Button children
+   */
+  children?: React.ReactNode;
 }
 
-const Button = ({ children }: IButtonProps) => {
-  return (
-    <button className='uppercase text-white border border-white p-1 active-border focus-visible-border'>
-      {children}
-    </button>
-  );
-};
+const Button = ({ children }: IButtonProps) => (
+  <button className='uppercase font-bold text-white border border-white px-4 py-2 active-border focus-visible-border'>
+    {children}
+  </button>
+);
 
 export default Button;
